eac089346aec800d8c3bcda35f19283dbdecdf1f
Manjeet Thadani <manjeet.thadani@gmail.com>
2021-02-11 12:54:42 +0530

    Remove todo

--- app/services/computer_player.rb
+++ app/services/computer_player.rb
@@ -1,6 +1,5 @@
 class ComputerPlayer
   def self.updated_board_state(game)
-    # TODO: use ComputerMove here
     computer_move = ComputerMove.new
     ai_decision = computer_move.negamax(game.board, game.player_1_marker, game.player_2_marker)
     game.board.place_marker(game.computer, ai_decision)
